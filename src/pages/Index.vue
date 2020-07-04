cc56e491ab4bdb1fd59caab9742c3a1a838d4a37
Sergio Gtz <sergio.oscar.gb@gmail.com>
2020-07-04 18:44:46 -0500

    design-location, weather desc and temperature

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -17,6 +17,20 @@
         </template>
       </q-input>
     </div>
+    <div class="col text-white text-center">
+      <div class="text-h4 text text-weight-light">
+        Manchester
+      </div>
+      <div class="text-h6 text-weight-light">
+        Rain
+      </div>
+      <div class="text-h1 text-weight-thin q-my-lg relative-position">
+        <span>8</span>
+        <span class="text-h4 relative-position degree">
+          &deg;
+        </span>
+      </div>
+    </div>
   </q-page>
 </template>
 
@@ -34,5 +48,6 @@ export default {
 <style lang="sass" >
   .q-page
     background: linear-gradient(to bottom, #267871, #136a8a)
-
+  .degree
+    top: -44px
 </style>
