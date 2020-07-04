55014e1e81dd7b35026744bbfe992ba90bed182e
Sergio Gtz <sergio.oscar.gb@gmail.com>
2020-07-04 10:26:20 -0500

    Design search bar

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,12 +1,33 @@
 <template>
   <q-page class="flex column">
+    <div class="col q-pt-lg q-px-md">
+      <q-input
+        v-model="search"
+        placeholder="Search"
+        dark
+        borderless
+        >
 
+        <template v-slot:before>
+          <q-icon name="my_location" />
+        </template>
+
+        <template v-slot:append>
+          <q-btn round dense flat icon="search" />
+        </template>
+      </q-input>
+    </div>
   </q-page>
 </template>
 
 <script>
 export default {
-  name: 'PageIndex'
+  name: 'PageIndex',
+  data() {
+    return {
+      search: ''
+    }
+  }
 }
 </script>
 
